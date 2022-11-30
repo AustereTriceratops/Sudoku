bcdce0a5fcf5889d14ea11d8d5306e82d5c37516
AustereTriceratops <gatewaylevi@gmail.com>
2022-11-30 02:12:14 -0600

    organizing file structure for three.js code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        kill suck destroy
-      </header>
+      <div className="canvas" id="canvas"></div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,4 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const element = screen.getByText("kill suck destroy");
-  expect(element).toBeInTheDocument();
 });
